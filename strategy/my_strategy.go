573b8963ff65e50b18c471ad5b8f3b5bd9ac020d
Tomohisa Ota <ota@ai2-jp.com>
2018-12-19 22:16:38 +0900

    What if number is random...
    It uses go channel to communicate.
    So I think it is cheating...

--- strategy/my_strategy.go
+++ strategy/my_strategy.go
@@ -4,21 +4,90 @@ import "github.com/tarao/prisoners-switch/rule"
 
 // MyNewStrategy returns a new strategy
 func MyNewStrategy() rule.Strategy {
-	return &myStrategy{}
+	register := make(chan int)
+	isCounter := make(chan bool)
+	go func(register chan int, isCounter chan bool) {
+		counterNumber := <-register
+		isCounter <- true
+		for true {
+			number := <-register
+			isCounter <- number == counterNumber
+		}
+
+	}(register, isCounter)
+
+	return &myStrategy{register, isCounter}
 }
 
 type myStrategy struct {
+	register  chan int
+	isCounter chan bool
 }
 
+// What if number is not sequence?
 func (s *myStrategy) NewPrisoner(number int, shout chan rule.Shout) rule.Prisoner {
-	return &prisoner{shout: shout}
+	return &prisoner{number: number, register: s.register, isCounter: s.isCounter, shout: shout}
 }
 
 type prisoner struct {
-	shout chan rule.Shout
+	number    int
+	register  chan int
+	isCounter chan bool
+	shout     chan rule.Shout
+
+	isInitialized bool
+	counter       int
+
+	haveSeenOn bool
+	switched   bool
 }
 
 func (p *prisoner) Enter(room rule.Room) {
-	// shout triumph when you think it's ready
-	p.shout <- rule.Triumph
+	if rule.TotalPrisoners == 1 {
+		// Am I the only one?
+		p.shout <- rule.Triumph
+		return
+	}
+	p.register <- p.number
+	iAmCounter := <-p.isCounter
+	aIsOn := room.TakeSwitchA().State()
+	if !iAmCounter {
+		if p.switched {
+			return;
+		}
+		if aIsOn {
+			p.haveSeenOn = true
+		} else {
+			if p.haveSeenOn {
+				room.TakeSwitchA().Toggle()
+				p.switched = true
+			}
+		}
+		return
+	}
+	// counter
+	if (!p.isInitialized) {
+		// make sure that intial state is 1!!
+		if (aIsOn) {
+			// intial state was 1
+			room.TakeSwitchA().Toggle()
+		} else {
+			/// intial state was 0
+			p.counter -= 1
+			room.TakeSwitchA().Toggle()
+		}
+		p.isInitialized = true
+		return
+	}
+	if (aIsOn) {
+		p.counter += 1;
+		if p.counter == rule.TotalPrisoners-1 {
+			p.shout <- rule.Triumph
+			return
+		}
+		room.TakeSwitchA().Toggle()
+	} else {
+		p.counter -= 1;
+		room.TakeSwitchA().Toggle()
+	}
 }
